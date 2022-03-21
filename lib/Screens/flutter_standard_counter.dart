77dd5ed7bf7b1e597a6bb70e4cce0a2a69e3ccd1
tex1 <ivanagrebenkin@gmail.com>
2022-03-21 21:52:09 +0700

    Переименован файл для виджетов в файл для тем.

--- lib/Screens/flutter_standard_counter.dart
+++ lib/Screens/flutter_standard_counter.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../Utils/widgets.dart';
+import '../Utils/themes.dart';
 
 
 
--- lib/Utils/themes.dart
+++ lib/Utils/themes.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 ThemeData themeOfStandardCounter() => ThemeData(primarySwatch: Colors.amber,);
+ThemeData themeOfHomePage() => ThemeData(primarySwatch: Colors.lightGreen,);
