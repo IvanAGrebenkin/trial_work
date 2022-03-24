783354532962b08b230dda2e0f9bf2bc9fea638d
tex1 <ivanagrebenkin@gmail.com>
2022-03-24 14:49:57 +0700

    На странице DropdownButton реализованы два выпадающих списка. Второй список активен для выбора только при условии выбора значения в первом списке.

--- lib/Screens/drop_down_button.dart
+++ lib/Screens/drop_down_button.dart
@@ -1,74 +1,85 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:trial_work/Utils/widgets.dart';
 
-import '../Utils/widgets.dart';
 
-class MyDropdownButton extends StatelessWidget {
+class MyDropdownButton extends StatefulWidget {
   const MyDropdownButton({Key? key}) : super(key: key);
 
-  // static const String _title = 'Flutter Code Sample';
+  @override
+  State<MyDropdownButton> createState() => _MyDropdownButtonState();
+}
 
-  get pageName => 'Кнопка DropdownButton';
+class _MyDropdownButtonState extends State<MyDropdownButton> {
+  get pageName => 'DropdownButton';
+
+
+
+  get items1 => <String>['...', 'Один', 'Два', 'Три', 'Четыре', ];
+  get items2 => <String>['...', 'Пять', 'Шесть', 'Семь', 'Восемь', ];
+
+  String dropdownValue1 = '...';
+  String dropdownValue2 = '...';
+
+  bool a = true;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // title: _title,
       home: Scaffold(
         appBar: appBar(context, pageName),
         drawer: navDrawer(context),
         body: Center(
           child: Column(
-            children: const [
-              SizedBox(height: 20,),
-              MyStatefulWidget(),
+            children: [
+              const SizedBox(height: 50,),
+              const Text('Выпадающий список №1:'),
+              DropdownButton(
+                value: dropdownValue1,
+                  items: items1.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );}).toList(),
+                onChanged: (String? newValue){ setState(() {
+                  dropdownValue1 = newValue!;
+                  if (newValue == '...') {dropdownValue2 = '...'; a=true;} else{a=false;}
+                });},
+                // onChanged: (String? newValue){ setState(() {
+                //   dropdownValue1 = newValue!;
+                // });},
+              ),
+              const SizedBox(height: 50,),
+              const Text('Выпадающий список №2:'),
+              IgnorePointer(
+                ignoring: a,
+                child: DropdownButton(
+                  value: dropdownValue2,
+                  items: items2.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );}).toList(),
+                  onChanged: (String? newValue){ setState(() {
+                    dropdownValue2 = newValue!;
+                  });},
+                ),
+              ),
+              const SizedBox(height: 50,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                child: Text('В списке 1 выбранно: "$dropdownValue1",',
+                  textAlign: TextAlign.center,),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                child: Text('а в списке 2 выбрано: "$dropdownValue2"',
+                  textAlign: TextAlign.center,),
+              ),
             ],
           ),
         ),
       ),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  String dropdownValue = '...';
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      hint: const SizedBox(width: 100,),
-      // disabledHint: const SizedBox(width: 50,),
-      focusColor: Colors.lightGreen,
-      dropdownColor: Color(0xFFFFCCBC), // фон выпавшего списка
-      autofocus: false,
-      alignment: AlignmentDirectional.centerStart,
-      value: dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String? newValue) {
-        setState(() {
-          dropdownValue = newValue!;
-        });
-      },
-      onTap:(){},
-      items: <String>['...','Один', 'Два', 'Три', 'Четыре.........']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
     );
   }
 }
