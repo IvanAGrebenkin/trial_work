1e7aa255a4fd3d5e468f92f5d8d6fab4989a64f6
tex1 <ivanagrebenkin@gmail.com>
2022-03-25 12:00:21 +0700

    Добавлена библиотеки Provider и SharedPreferences.

--- lib/Screens/drop_down_button.dart
+++ lib/Screens/drop_down_button.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:trial_work/Utils/widgets.dart';
 
@@ -11,16 +10,39 @@ class MyDropdownButton extends StatefulWidget {
 }
 
 class _MyDropdownButtonState extends State<MyDropdownButton> {
+
   get pageName => 'DropdownButton';
 
-  get items1 => <String>['...', 'Один', 'Два', 'Три', 'Четыре', ];
-  get items2 => <String>['...', 'Пять', 'Шесть', 'Семь', 'Восемь', ];
+  List <String> groups = <String>['...', 'Кастрюли', 'Чайники', 'Хозяйственные изделия', 'Плоские изделия', ];
+  List <String> pans = <String>['...', 'Кастрюля с ободком', 'Кастрюля без ободка', 'Позница', ];
+  List <String> teapots = <String>['...', 'Чайник цельнотянутый', 'Чайник с закатным дном', 'Заварник', ];
+  List <String> householdProducts = <String>[
+    '...',
+    'Кружка',
+    'Бак',
+    'Ведро сварное',
+    'Ведро цельнотянутое',
+    'Ковш',
+    'Ковш с крышкой',
+    'Дуршлаг',
+    'Бидон',
+    'Горшок ночной',
+  ];
+
+  get items1 => groups;
+
+  get items2 => pans;
 
   String dropdownValue1 = '...';
   String dropdownValue2 = '...';
+  bool ignore = true;
 
-  bool a = true;
 
+
+
+
+
+  // if (dropdownValue1 == 'Кастрюли') { List <String> items2 => <String>['...', 'Пять', 'Шесть', 'Семь', 'Восемь', ];}
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,7 +53,7 @@ class _MyDropdownButtonState extends State<MyDropdownButton> {
           child: Column(
             children: [
               const SizedBox(height: 50,),
-              const Text('Выпадающий список №1:'),
+              const Text('Группа изделий:'),
               DropdownButton(
                 value: dropdownValue1,
                   items: items1.map<DropdownMenuItem<String>>((String value) {
@@ -41,17 +63,17 @@ class _MyDropdownButtonState extends State<MyDropdownButton> {
                       );}).toList(),
                 onChanged: (String? newValue){ setState(() {
                   dropdownValue1 = newValue!;
-                  if (newValue == '...') {dropdownValue2 = '...'; a=true;} else{a=false;}
+                  if (newValue == '...') {dropdownValue2 = '...'; ignore=true;}
+                  else { ignore=false;}
                 });},
               ),
               const SizedBox(height: 50,),
-              const Text('Выпадающий список №2:'),
+              const Text('Наименование изделия:'),
               IgnorePointer(
-                ignoring: a,
+                ignoring: ignore,
                 child: DropdownButton(
                   value: dropdownValue2,
-                  items: items2.map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
+                  items: items2.map<DropdownMenuItem<String>>((String value) {return DropdownMenuItem<String>(
                       value: value,
                       child: Text(value),
                     );}).toList(),
@@ -61,14 +83,12 @@ class _MyDropdownButtonState extends State<MyDropdownButton> {
                 ),
               ),
               const SizedBox(height: 50,),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 50.0),
-                child: Text('В списке 1 выбранно: "$dropdownValue1",',
-                  textAlign: TextAlign.center,),
+              Text('Группа изделий: "$dropdownValue1",',
+                textAlign: TextAlign.start,
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 50.0),
-                child: Text('а в списке 2 выбрано: "$dropdownValue2"',
+                child: Text('Наименование изделия: "$dropdownValue2"',
                   textAlign: TextAlign.center,),
               ),
             ],
